e8546652198c6eb3c0871a6812c199023da46b3a
lkmodibbo <lukmansaidmodibbo.com>
2022-09-14 02:08:56 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Description: prints _putchar using putchar prototype
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void) 
+{
+int ch;
+for (ch = 'a'; ch <= 'z'; ++ch)
+_putchar(ch);
+_putchar('\n'); 
+}
