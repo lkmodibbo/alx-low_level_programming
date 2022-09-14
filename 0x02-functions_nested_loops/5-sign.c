aa5c55ad794644d76bdc86b2162320812b4ea69d
lkmodibbo <lukmansaidmodibbo.com>
2022-09-14 04:15:56 -0700

    sign 5.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -7,19 +7,20 @@
  * Return: 1 if +, 0 if 0 and -1 if -
  */
 int print_sign(int n)
-{ 
+{
 if (n > 0)
 {
 _putchar(43 + 0);
 return (1);
 }
-else if (n == 0){
- _putchar(0 + 48);
+else if (n == 0) 
+{
+_putchar(0 + 48);
 return (0);
 }
-else    
+ else
 {
-_putchar(45 + 0);    
+_putchar(45 + 0);
 return (-1);     
 }
 }
