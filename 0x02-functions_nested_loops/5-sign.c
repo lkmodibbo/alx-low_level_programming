b4f98cea00c03238eeda1bcc60473c1aa8c925b2
lkmodibbo <lukmansaidmodibbo.com>
2022-09-14 04:19:39 -0700

    sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -13,14 +13,14 @@ if (n > 0)
 _putchar(43 + 0);
 return (1);
 }
-else if (n == 0) 
+else if (n == 0)
 {
 _putchar(0 + 48);
 return (0);
 }
- else
+else
 {
 _putchar(45 + 0);
-return (-1);     
+return (-1);
 }
 }
