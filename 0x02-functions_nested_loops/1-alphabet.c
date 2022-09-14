5416b0aba877268f852c03bda5e8c4cabf23e152
lkmodibbo <lukmansaidmodibbo.com>
2022-09-14 02:12:53 -0700

    1-aplalphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,10 +6,10 @@
  *
  * Return: Always 0 (Success)
  */
-void print_alphabet(void) 
+void print_alphabet(void)
 {
 int ch;
 for (ch = 'a'; ch <= 'z'; ++ch)
 _putchar(ch);
-_putchar('\n'); 
+ _putchar('\n');
 }
