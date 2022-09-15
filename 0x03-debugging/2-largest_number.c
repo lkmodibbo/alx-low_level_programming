79cf81f128b94b2da13cd8e0851ca72462da2284
lkmodibbo <lukmansaidmodibbo.com>
2022-09-15 14:37:18 -0700

    task 2

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,27 +1,106 @@
 #include "main.h"
 
+
+
 /**
- * largest_number - returns the largest of 3 numbers
+
+ * largest_number - returns the largest of three numbers
+
  * @a: first integer
+
  * @b: second integer
+
  * @c: third integer
+
  * Return: largest number
+
  */
 
+
+
 int largest_number(int a, int b, int c)
+  
 {
-int largest;
-if (a >= b && b >= c)
-{
-largest = a;
+  
+  int largest;
+  
+
+  
+  if (a >= b && a >= c)
+    
+    {
+      
+      largest = a;
+      
+    }
+  
+  else if (b >= a && b >= c)
+    
+    {
+      
+      largest = b;
+      
+    }
+  
+  else
+    
+    {
+      
+      largest = c;
+      
+    }
+  
+  return (largest);
+  
 }
-else if (b >= a && a >= c)
+
+
+
+TASK 4
+
+3-print_remaining_days.c
+
+#include <stdio.h>
+
+#include "main.h"
+
+
+
+/**
+
+ * print_remaining_days - collects a date and prints how many days are 
+
+ * left in the year, including leap years
+
+ * @month: month in number format
+
+ * @day: day of month
+
+ * @year: year
+
+ * Return: void
+
+ */
+
+void print_remaining_days(int month, int day, int year)
 {
-largest = b;
-}
-else
+ if ((year % 4 == 0) && (year % 400 == 0 || year % 100 != 0))    
+{      
+if (month >= 3 && day >= 60)	
 {
-largest = c;
+day++;
+ }     
+ printf("Day of the    printf("Remaining days: %d\n", 366 - day);
 }
-return (largest);
+else    
+if (month == 2 && day == 60)
+{  
+printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);  
+}  
+ else
+{
+ printf("Day of the year: %d\n", day);
+printf("Remaining days: %d\n", 365 - day);
+}      
+} 
 }
