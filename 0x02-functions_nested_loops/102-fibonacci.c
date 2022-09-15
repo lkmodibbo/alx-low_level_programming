9759455684e11c9372aa7a6918c3fada5ad6711f
lkmodibbo <lukmansaidmodibbo.com>
2022-09-14 17:00:42 -0700

    fibonaci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,23 +1,12 @@
 #include"main.h"
-
-
-
 /**
-
  * main - Entry point
-
  *
-
  * Description: Prints the sum of even-valued
-
  *Fibonacci sequence not exceed
-
  *4million
-
  *
-
  * Return: Always 0 (Success)
-
  */
 int main(void)
 {
