00be5a5afeff2cb850dde6dd35642a0c784e0087
lkmodibbo <lukmansaidmodibbo.com>
2022-09-16 09:07:04 -0700

    square_numbers

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * print_square - Print a square with #.
- * @size: Is the size of the square.
- *
- * Return: To the value of void..
- */
+* print_square - Print a square with #.
+* @size: Is the size of the square.
+*
+* Return: To the value of void
+*/
 void print_square(int size)
 {
 int i;
@@ -15,16 +15,15 @@ if (size <= 0)
 {
 putchar('\n');
 }
-else   
-{     
-while (i < size)	
-{  
+else
+{while (i < size)	
+{
 for (j = 0; j < size; j++)
-{     
+{
 _putchar('#');
 }
 i++;
 _putchar('\n');
-}     
+}
 }
 }
