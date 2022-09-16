8e0ff15b7beb9ea3d07ad47b972b7c2a01218313
lkmodibbo <lukmansaidmodibbo.com>
2022-09-16 04:53:38 -0700

    pritn_line.c git commit -m pritn_line.c git commit -m pritn_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,29 +1,17 @@
-#include"main.h"
-
-
+#include <stdio.h>
+#include "main.h"
 
 /**
-
  * print_line - print a straight line
-
  *
-
  * @n: is the number of times the _ character
-
  *     should be printed
-
  */
 
-
-
-void print_line(int n)
-  
+void print_line(int n)  
 {
-  
-  int lnChr;
-  
-
-  
+  int lnChr; 
+ 
   if (n <= 0)
     
     _putchar('\n');
