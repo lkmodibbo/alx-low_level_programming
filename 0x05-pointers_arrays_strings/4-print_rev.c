3d5f6162c0f36fc48700ee52079db504b4209be7
lkmodibbo <lukmansaidmodibbo.com>
2022-09-20 11:08:38 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -11,6 +11,7 @@ void print_rev(char *s)
 int index;
 /*finds the length of string without null character*/
 for (index = 0; s[index] != '\0'; ++index);
+/*print char from the last index as you decrement*/
 for (--index; index >= 0; --index)
 _putchar(s[index]);
 _putchar('\n');
