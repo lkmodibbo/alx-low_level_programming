369570c3500710d0e93317273302c90a39c04ddc
lkmodibbo <lukmansaidmodibbo.com>
2022-09-21 15:16:03 -0700

    char 0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *main - check the code
+ *
+ *Return: Always 0
+ */
+char *_strcat(char *dest, char *src);
+{
+int index = 0, dest_len = 0;
+while(dest[index++])
+dest_len++
+for (index = 0; src[index]; index++)
+dext[dest_len++] = src[index];
+return (dest);
+}
