39766455d4ad127ae9dacf337508a965ca9c1598
lkmodibbo <lukmansaidmodibbo.com>
2022-09-21 19:31:04 -0700

    task 2

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strcat - concatenate two strings but add inputted number of bytes
+ *@dest: string to be appended upon
+ *@src:  string to be completed at the end of dest
+ *@n: integer parameter to compare index to
+ *Return: return new concatenated string
+ */
+char *_strncat(char *dest, char *src, int n);
+{
+index = 0. dest_len = 0;
+while (dest_[index++])
+ dest_len++;
+for (index = 0; src[index] && index < n; index++)
+dest[dest_len++] = src[index];
+return (dest);
+}
