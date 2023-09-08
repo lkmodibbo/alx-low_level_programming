29db146aa787b301f1d275b81113c888a1709853
lkmodibbo <lukmansaidmodibbo@gmail.com>
2023-09-08 16:42:55 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+* main - Entry point
+* Return: Always 0 (success)
+*/
+int main(void)
+int d;
+int p;
+for (d = '0'; d < '9' d++)
+{
+for (p = d + 1; P <= '9' p++)
+{
+putchar(d);
+putchar(p);
+return (0);
+}
