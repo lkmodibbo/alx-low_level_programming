77e1b292e89a4f8f4bcf36f7898e2fecfe45837d
lkmodibbo <lukmansaidmodibbo@gmail.com>
2023-09-08 05:17:29 -0700

    positive or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -6,7 +6,7 @@
 * Description - Return value of main
 * Return - always 0 (success)
 */
-int main(void) /** Description - Return value of main */
+int main(void) /*return main*/
 {
 int n;
 srand(time(0));
