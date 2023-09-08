fd211e2a4f293404bce7a6fe1b8e5e38fad0ae3e
lkmodibbo <lukmansaidmodibbo@gmail.com>
2023-09-08 16:57:57 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,18 +8,18 @@
 int main(void)
 int d;
 int p;
-for (d = '0'; d < '9' d++)
+for(d = '0'; d < '9'; d++)
 {
-for (p = d + 1; P <= '9' p++)
+for(p = d + 1; P <= '9'; p++)
 {
-if (p !=d)
+if(p !=d)
 {
 putchar(d);
 putchar(p);
 if (d == '8' && p == '9')
 continue;
 putchar(',');
-putcjar(' ');
+putchar(' ');
 }
 }
 }
