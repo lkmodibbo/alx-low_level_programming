ea72b8ead6056c9e402328cb9382460fdf5d44de
lkmodibbo <lukmansaidmodibbo@gmail.com>
2023-09-08 17:11:04 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,11 +9,11 @@ int main(void)
 {
 int d;
 int b;
-for(d = '0'; d < '9'; d++)
+for (d = '0'; d < '9'; d++)
 {
-for(b = d + 1; b <= '9'; b++)
+for (b = d + 1; b <= '9'; b++)
 {
-if(b != d)
+if (b != d)
 {
 putchar(d);
 putchar(b);
