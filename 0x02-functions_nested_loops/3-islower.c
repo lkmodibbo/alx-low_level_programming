9f729cb16c22bfaea952d1c49dae8baba07dbd22
lkmodibbo <lukmansaidmodibbo@gmail.com>
2023-09-13 05:38:45 -0700

    3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,13 +2,13 @@
 /**
 * _slower - checks for the lowercase character
 * @c - the character to be checked
-*Return: 1 for lowercase character 0 for anything else
+*Return:1 for lowercase character or 0 for anything else
 */
 int _islower(int c)
 {
 if (c >= 97 && c <= 122)
 {
-return(1);
+return (1);
 }
 return (0);
 }
