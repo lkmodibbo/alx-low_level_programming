e35472480405494cdc207fd2089842ce9456274a
lkmodibbo <lukmansaidmodibbo@gmail.com>
2023-09-18 05:42:01 -0700

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,6 +1,6 @@
 int _putchar(char c);
-voice reset_to_98(int *n);
-voice swap_int(int *a, int *b);
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
 void print_rev(char *s);
